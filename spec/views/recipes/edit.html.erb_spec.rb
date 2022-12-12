6cd53b03bf7fee409afc0a4878582d20529e20d6
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-12 21:48:13 +0000

    rubocop -A

--- spec/views/recipes/edit.html.erb_spec.rb
+++ spec/views/recipes/edit.html.erb_spec.rb
@@ -1,37 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe "recipes/edit", type: :view do
-  let(:recipe) {
+RSpec.describe 'recipes/edit', type: :view do
+  let(:recipe) do
     Recipe.create!(
-      name: "MyString",
+      name: 'MyString',
       preparation_time_seconds: 1,
       cooking_time_seconds: 1,
-      description: "MyText",
+      description: 'MyText',
       public: false,
       user: nil
     )
-  }
+  end
 
   before(:each) do
     assign(:recipe, recipe)
   end
 
-  it "renders the edit recipe form" do
+  it 'renders the edit recipe form' do
     render
 
-    assert_select "form[action=?][method=?]", recipe_path(recipe), "post" do
+    assert_select 'form[action=?][method=?]', recipe_path(recipe), 'post' do
+      assert_select 'input[name=?]', 'recipe[name]'
 
-      assert_select "input[name=?]", "recipe[name]"
+      assert_select 'input[name=?]', 'recipe[preparation_time_seconds]'
 
-      assert_select "input[name=?]", "recipe[preparation_time_seconds]"
+      assert_select 'input[name=?]', 'recipe[cooking_time_seconds]'
 
-      assert_select "input[name=?]", "recipe[cooking_time_seconds]"
+      assert_select 'textarea[name=?]', 'recipe[description]'
 
-      assert_select "textarea[name=?]", "recipe[description]"
+      assert_select 'input[name=?]', 'recipe[public]'
 
-      assert_select "input[name=?]", "recipe[public]"
-
-      assert_select "input[name=?]", "recipe[user_id]"
+      assert_select 'input[name=?]', 'recipe[user_id]'
     end
   end
 end
