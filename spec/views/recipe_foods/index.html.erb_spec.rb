bff68616e4eec644e0923cd090ca92ef3f03a7b1
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-12 22:54:40 +0000

    Fix linters

--- spec/views/recipe_foods/index.html.erb_spec.rb
+++ spec/views/recipe_foods/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'recipe_foods/index', type: :view do
-  let!(:food) { Food.create(name: 'food name',user: User.create) }
-  let!(:recipe) { Recipe.create(name: 'recipe name',user: User.create) }
+  let!(:food) { Food.create(name: 'food name', user: User.create) }
+  let!(:recipe) { Recipe.create(name: 'recipe name', user: User.create) }
 
   before(:each) do
     assign(:recipe_foods, [
@@ -23,7 +23,7 @@ RSpec.describe 'recipe_foods/index', type: :view do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     # assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(food.id.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(recipe.id.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(food.name.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(recipe.name.to_s), count: 2
   end
 end
