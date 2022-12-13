c1c32b33ddab26832d2ca5e4e7c141ea24f345fe
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-13 16:25:41 +0000

    Minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  
+
+  root "foods#index"
 end
