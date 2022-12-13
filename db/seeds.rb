40ee9ab454cf8031947d78df4b5b69912a4121b7
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-13 20:07:06 +0000

    Add userr and food seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,78 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 users = User.create([
-    {name: 'Ruth Bradley'},
-    {name: 'Leticia Barrett'},
-    {name: 'Kaiden Carlson'},
-    {name: 'Aubree Rojas'},
-    {name: 'Trevon Rich'},
+    {
+        name: 'Ruth Bradley',
+        email: 'ruth_bradley@testmail.com',
+        password: 'ruth_bradley_pass'
+    },
+    {
+        name: 'Leticia Barrett',
+        email: 'leticia_barrett@testmail.com',
+        password: 'leticia_barrett_pass'
+    },
+    {
+        name: 'Kaiden Carlson',
+        email: 'kaiden_carlson@testmail.com',
+        password: 'kaiden_carlson_pass'
+    },
+    {
+        name: 'Aubree Rojas',
+        email: 'aubree_rojas@testmail.com',
+        password: 'aubree_rojas_pass'
+    },
+    {
+        name: 'Trevon Rich',
+        email: 'trevon_rich@testmail.com',
+        password: 'trevon_rich_pass'
+    },
 ])
 
-foods = Food.create([
 
-])
+def user_create_foods_and_recipes(user_in)
+    Food.create([
+        {
+            name: "sausage",
+            unit: "",
+            price: "1.1",
+            quantity: 2,
+            user: user_in
+        },
+        {
+            name: "Mayonnaise",
+            unit: "ml",
+            price: "2.2",
+            quantity: 500,
+            user: user_in
+        },
+        {
+            name: "Onion",
+            unit: "g",
+            price: "3.3",
+            quantity: 30,
+            user: user_in
+        },
+        {
+            name: "Ketchup",
+            unit: "ml",
+            price: "4.4",
+            quantity: 300,
+            user: user_in
+        },
+        {
+            name: "Cabbage",
+            unit: "g",
+            price: "5.5",
+            quantity: 100,
+            user: user_in
+        }
+    ])
 
+    Recipe.create(
+        
+    )
+end
 
-
+users.each do |user|
+    user_create_foods_and_recipes user
+end
