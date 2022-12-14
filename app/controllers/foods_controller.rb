0f0055dbb3460800a74e5ffe55c79977ea167531
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-14 23:59:40 +0000

    Add ingredint to recipe details

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[show edit update destroy]
+  before_action :set_food, only: %i[show destroy]
 
   # GET /foods or /foods.json
   def index
@@ -14,9 +14,6 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
-  # GET /foods/1/edit
-  def edit; end
-
   # POST /foods or /foods.json
   def create
     @food = Food.new(food_params)
@@ -27,19 +24,6 @@ class FoodsController < ApplicationController
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @food.errors, status: :unprocessable_entity }
       end
     end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :RecipeFood
+  has_many :recipeFood
   has_many :ingredients, through: :RecipeFood, source: :food
 
   def self.publics
