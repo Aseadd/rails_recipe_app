1103be01ad867b630d56ec6984ee781829f6d9be
addis <betsega23@gmail.com>
2022-12-16 01:08:21 +0300

    Add foriegn key validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :recipe
-  has_many :food
+  has_many :recipe, dependent: :destroy
+  has_many :food, dependent: :destroy
+
+  validates :name, presence: true
+
 end
