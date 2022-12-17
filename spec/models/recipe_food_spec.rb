107afd0bbe07e02844509f2b6df13cbe4a4cf603
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-17 18:43:23 +0000

    Add recipe food test

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) do
+    User.create(
+      name: 'Kaiden Carlson',
+      email: 'kaiden_carlson@testmail.com',
+      password: 'kaiden_carlson_pass'
+    )
+  end
+
+  let!(:food) do
+    Food.new(
+      name: 'Tomatoes',
+      unit: 'piece',
+      price: '1.1',
+      quantity: 0,
+      user:
+    )
+  end
+
+  let!(:recipe) do
+    Recipe.create(
+      name: 'test',
+      preparation_time_seconds: 10.minute.second,
+      cooking_time_seconds: 5.minute.second,
+      description: 'desciption test recipe',
+      public: true,
+      user:
+    )
+  end
+
+  let!(:recipe_food) do
+    RecipeFood.create(
+      amount: 1,
+      food:,
+      recipe:
+    )
+  end
+
+  it 'valid with all correct atributes' do
+    expect(recipe_food).to be_valid
+  end
+
+  it 'invalid if amount is nill' do
+    recipe_food.amount = nil
+    expect(recipe_food).to_not be_valid
+  end
+  it 'invalid if food is nill' do
+    recipe_food.food = nil
+    expect(recipe_food).to_not be_valid
+  end
+  it 'invalid if recipe is nill' do
+    recipe_food.recipe = nil
+    expect(recipe_food).to_not be_valid
+  end
 end
