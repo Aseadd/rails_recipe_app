c3d84de7545cbd246ef24bbad0f5974c5edcfa78
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-17 22:45:45 +0000

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?F
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -32,6 +32,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  config.hosts << ENV["RAILWAY_STATIC_URL"] 
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
